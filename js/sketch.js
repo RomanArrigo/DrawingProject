8e6144837a6aab1a30edd173dbaa0ec56ba676c6
RomanArrigo <93398234+RomanArrigo@users.noreply.github.com>
2021-11-26 17:14:50 +0000

    Added Brush/Paint Tools

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,50 @@
-function setup() {
-    var canvas = createCanvas(800, 800);
-    canvas.parent("p5container");
-}
+var brushSize, brushShape, paintColour;
+var coloursArray= ['red','green','blue'];
 
-function draw() {
+function setup(){
+    var canvas = createCanvas(800, 450);
+    canvas.parent("p5container");
+    brushSize = 10;
     background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    paintColour = coloursArray[0];
+
+}
+
+function draw(){
+
+    if(paintColour === 'red'){
+        fill('rgb(255,0,0)');  
+
+    } else if(paintColour === 'green'){
+        fill('rgb(0,255,0)')
+
+    } else if(paintColour === 'blue'){
+        fill('rgb(0,0,255)')
+    }
+
+    noStroke();
+
+
+    if(mouseIsPressed == true){
+        ellipse(mouseX, mouseY, brushSize);
+    };
+
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW && brushSize <= 50){
+        brushSize = brushSize + 5;
+    } else if(keyCode === DOWN_ARROW && brushSize >= 10) {
+        brushSize = brushSize - 5;
+    }    
+
+    if(keyCode === 81){
+        paintColour = coloursArray[0]
+    }    
+
+    if(keyCode === 87){
+        paintColour = coloursArray[1]
+    }    
+
 }
